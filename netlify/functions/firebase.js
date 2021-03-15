ece5bd488b9994a40cac4a104be4574a1ae43b06
Adrian Hollander <adrian.hollander@kellogg.northwestern.edu>
2021-03-14 21:53:58 -0500

    Netlify Effort

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,13 +1,13 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {apiKey: "AIzaSyAqL-ZHu1F-zkQiOw8sMA1Tpsk7YgS0o6E",
+const firebaseConfig = {
+apiKey: "AIzaSyAqL-ZHu1F-zkQiOw8sMA1Tpsk7YgS0o6E",
 authDomain: "kelloggbuddies.firebaseapp.com",
 projectId: "kelloggbuddies",
 storageBucket: "kelloggbuddies.appspot.com",
 messagingSenderId: "339327388559",
 appId: "1:339327388559:web:6a53f95fde1d078fc1d9fa"
-
 }
 
 if (!firebase.apps.length) {
