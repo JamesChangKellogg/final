e023b2d427ac7221d9748be2c39b653492213e9a
Adrian Hollander <adrian.hollander@kellogg.northwestern.edu>
2021-03-15 01:11:36 -0500

    Updates

--- netlify/functions/get_matches.js
+++ netlify/functions/get_matches.js
@@ -2,10 +2,27 @@ let firebase = require('./firebase')
 
 exports.handler = async function(event) {
   
-  let matchesdata = []
+  let matchesData = []
+  let db = firebase.firestore()
+  let querySnapshot = await db.collection('savedMatches').get()
+  let savedMatches = querySnapshot.docs
+
+  for (let i=0; i<savedMatches.length; i++){
+  let savedID = savedMatches[i].id
+  let saved = savedMatches[i].data()
+
+  matchesData.push({
+    id: savedID,
+    text: saved.matchName
+  })
+}
+
+  // let querySnapshot = await db.collection('savedMatches')
+  //                             .where('userID', '==', userID).orderBy('matchID').get()
+  // let userID
   
   return {
     statusCode: 200,
-    body: JSON.stringify(matchesdata)
+    body: JSON.stringify(matchesData)
   }
 }
